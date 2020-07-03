3ab94320154e0e40f5e8bf5f35b298494b031358
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-03 14:51:45 -0400

    Added purchuse auto clicker.

--- src/js/App.js
+++ src/js/App.js
@@ -3,19 +3,54 @@ import {DonutMaker} from "./DonutMaker.js";
 const donutMaker = new DonutMaker();
 
 const donutMakerClickerButton = document.getElementById("makeDonutButton");
-const madeDonutsNumber = document.getElementById("madeDonuts");
+const madeDonutsNumberElement = document.getElementById("madeDonuts");
+const purchaseAutoClickerButton = document.getElementById("purchaseAutoClickerButton");
+const purchasedAutoClickersElement = document.getElementById("purchasedAutoClickers");
+const autoClickerPriceElement = document.getElementById("autoClickerPrice");
 
 initClickerButton();
+initPurchaseAutoClickerButton();
 
 
 function initClickerButton() {
-    madeDonutsNumber.innerText = donutMaker.getClicksNumber();
+    autoClickerPriceElement.innerHTML = donutMaker.getAutoClickerCost();
+    madeDonutsNumberElement.innerText = donutMaker.getClicksNumber();
 
     donutMakerClickerButton.addEventListener('click', () => {
         donutMaker.makeDonut();
-        madeDonutsNumber.innerText = donutMaker.getClicksNumber();
+        madeDonutsNumberElement.innerText = donutMaker.getClicksNumber();
+
+        if (donutMaker.getClicksNumber() >= donutMaker.getAutoClickerCost()) {
+            enableBtn("purchaseAutoClickerButton")
+        }
     });
 }
+
+function initPurchaseAutoClickerButton() {
+    purchaseAutoClickerButton.addEventListener('click', () => {
+        donutMaker.updateDonutsNumberAfterPurchaseAutoClicker();
+        madeDonutsNumberElement.innerText = donutMaker.getClicksNumber();
+
+        donutMaker.updateNumberOfPurchasedAutoClickers();
+        purchasedAutoClickersElement.innerHTML = donutMaker.getAutoClickersNumber();
+
+        donutMaker.updateAutoClickerCost();
+        autoClickerPriceElement.innerHTML = donutMaker.getAutoClickerCost();
+
+        if (donutMaker.getClicksNumber() < donutMaker.getAutoClickerCost()) {
+            disableBtn("purchaseAutoClickerButton")
+        }
+    });
+}
+
+function enableBtn(buttonElementId) {
+    document.getElementById(buttonElementId).disabled = false;
+}
+
+function disableBtn(buttonElementId) {
+    document.getElementById(buttonElementId).disabled = true;
+}
+
 
 
 
--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -5,7 +5,7 @@ class DonutMaker {
     constructor() {
         this.clicksNumber = 0;
         this.autoClickerCost = 10;
-        this.autoClickerClicksNumber = 0;
+        this.autoClickersNumber = 0;
         this.multiplierCost = 10;
         this.multiplierNumber = 0;
     }
@@ -14,7 +14,27 @@ class DonutMaker {
         this.clicksNumber += 1;
     }
 
+    updateDonutsNumberAfterPurchaseAutoClicker() {
+        this.clicksNumber = this.clicksNumber - this.autoClickerCost;
+    }
+
+    updateNumberOfPurchasedAutoClickers() {
+        this.autoClickersNumber += 1;
+    }
+
+    updateAutoClickerCost() {
+        this.autoClickerCost = this.autoClickerCost + this.autoClickerCost * 0.1;
+    }
+
     getClicksNumber() {
         return this.clicksNumber;
+    }
+
+    getAutoClickerCost() {
+        return this.autoClickerCost;
+    }
+
+    getAutoClickersNumber() {
+        return this.autoClickersNumber;
     }
 }
